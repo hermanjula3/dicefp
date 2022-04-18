3ff201cf4d7045d62a39b3ea8bd8bb47016f609e
hermanjula3 <103096558+hermanjula3@users.noreply.github.com>
2022-04-19 06:23:47 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,11 @@
+<?php
+
+
+//Masukan Data User Agent
+$user_agent = "xxxx";
+//Maaukan Cookie
+$cookie = "xxxx";
+//Masukan Data Dice
+$data_dice = "xxxx";
+//Masukan Target Profit
+$profit = "0.00671818";
